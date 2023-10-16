66179d16e9b39b34921b974a411d98647c4c8153
Brynfor Morris <brynfor.morris@gmail.com>
2023-10-16 15:53:54 -0500

    soln progess

--- grokking_coding_solutions/warmups/contains_duplicate.py
+++ grokking_coding_solutions/warmups/contains_duplicate.py
@@ -4,5 +4,16 @@ test_case2 = [1,2,3,1]
 
 class Solution:
     def containsDuplicate(self, nums):
-      # TODO: Write your code here
-      return False
+      
+        target_num = None
+
+        for index, num in enumerate(nums):
+            target_num = num
+            # target_list = nums.slice(index)
+            print(target_list)
+
+
+    #   return False
+
+test = Solution()
+print(test.containsDuplicate(test_case1))
